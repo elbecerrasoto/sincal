09b39d357ac088b7b5dbc8152916709b6739c089
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-11-14 10:30:19 -0700

    add debugging template table viz

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(tidyverse)
 
+# globals ----
+
 TEMPLATE <- read_tsv("data/input_base.tsv")
 
 MXN_USD <- 18.5
@@ -8,19 +10,7 @@ DATE <- Sys.Date() # later make it reactive upon everything
 TEMPLATE$date <- DATE
 SECTORS <- unique(TEMPLATE$sector)
 
-mode_params <- tabsetPanel(
-  id = "mode_params",
-  type = "hidden",
-  tabPanel(
-    "mode_oridest",
-    selectInput("oridest_sector", "Selecciona el sector:", SECTORS),
-    numericInput("oridest_invest", "Ingresa el monto a invertir en USD:", 0)
-  ),
-  tabPanel(
-    "mode_shocks"
-  ),
-)
-
+# UI helpers ----
 
 select_mode <- radioButtons("template_mode",
   "Seleciona la plantilla de entrada a generar:",
@@ -32,17 +22,38 @@ select_mode <- radioButtons("template_mode",
 )
 
 
+mode_params <- tabsetPanel(
+  id = "mode_params",
+  type = "hidden",
+  tabPanel(
+    "mode_oridest",
+    selectInput("oridest_sector", "Selecciona el sector:", SECTORS),
+    numericInput("oridest_invest", "Ingresa el monto a invertir en USD:", 0, min = 0)
+  ),
+  tabPanel(
+    "mode_shocks"
+  ),
+)
+
+# UI ----
+
 ui <- fluidPage(
   select_mode,
   textInput("experimento", "Nombre del Experimento"),
-  numericInput("tipo_cambio", "Tipo de Cambio MXN a USD", value = MXN_USD),
-  mode_params
+  numericInput("tipo_cambio", "Tipo de Cambio MXN a USD", MXN_USD, min = 0),
+  mode_params,
+  dataTableOutput("template_tab")
 )
+
+# server ----
 
 server <- function(input, output) {
   observeEvent(input$template_mode, {
     updateTabsetPanel(inputId = "mode_params", selected = input$template_mode)
   })
+ 
+  output$template_tab <- renderDataTable(TEMPLATE)
+  
 }
 
 shinyApp(ui = ui, server = server)
